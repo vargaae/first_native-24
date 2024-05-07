69d7483ace33dc53f1f25c1fa0bbee651033c3ca
vargaae <vargaae@hotmail.com>
2024-05-07 02:47:37 +0200

    Init Aora project with React Native and Expo +Router

---
+++ app/index.jsx
@@ -0,0 +1,24 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>My first React Native App! -2024- AORA Project</Text>
+      <StatusBar style="auto" />
+      <Link href="profile" style={{ color: "blue" }}>
+        Go to Profile
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
