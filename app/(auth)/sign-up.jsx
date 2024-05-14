fc7ef70a7df597537913fd3e073494d4ed7a878d
vargaae <vargaae@hotmail.com>
2024-05-15 00:09:50 +0200

    SetUp File & Folder Structure

---
+++ app/(auth)/sign-up.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Sign Up = () => {
+  return (
+    <View>
+      <Text>Sign Up</Text>
+    </View>
+  )
+}
+
+export default Sign Up
